5783f365878491fbb69ec4a6066dce0eb65d0c92
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-01 20:17:58 +0530

    Add files via upload

---
+++ pyhton_study_1/page5/script.py
@@ -0,0 +1,11 @@
+# Assign 'Bob' to the name variable
+name = "Bob"
+
+# Print the value of the name variable
+print(name)
+
+# Assign 7 to the number variable
+number = 7
+
+# Print the value of the number variable
+print(number)
