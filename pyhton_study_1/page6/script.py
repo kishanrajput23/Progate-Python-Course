0371d21fbeecd596d8e6977f0b694f701f386088
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-02 19:44:23 +0530

    Add files via upload

---
+++ pyhton_study_1/page6/script.py
@@ -0,0 +1,10 @@
+apple_price = 2
+apple_count = 8
+
+# Assign the result of apple_price * apple_count to the total_price variable
+total_price = apple_price * apple_count
+print(total_price)
+
+
+# Print the value of the total_price variable
+
