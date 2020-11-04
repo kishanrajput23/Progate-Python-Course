4b4ada69942fef9cf8ea7f3d51cbf4fdf903fed7
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-04 20:34:35 +0530

    Add files via upload

---
+++ pyhton_study_1/page9/script.py
@@ -0,0 +1,8 @@
+age = 24
+# Print 'I am 24 years old' using the age variable
+print("I am" + str(age)  + "years old")
+
+count = '5'
+# Convert the count variable to an integer data type, add 1 to it, and print it
+y = int(count) + 1
+print(y)
