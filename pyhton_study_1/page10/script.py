58711b40266964270acb5f1cebee979720bc1d4b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-05 20:40:29 +0530

    Add files via upload

---
+++ pyhton_study_1/page10/script.py
@@ -0,0 +1,11 @@
+x = 7 * 10
+y = 5 * 6
+
+# if x equals 70, print 'x is 70'
+if x == 70:
+    print("x is 70")
+
+
+# if y does not equal 40, print 'y is not 40' 
+if y != 40:
+    print("y is not 40")
