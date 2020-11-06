e55786a52ef1560002475c08e6e0794afde3882f
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-06 16:52:51 +0530

    Add files via upload

---
+++ pyhton_study_1/page11/script.py
@@ -0,0 +1,13 @@
+x = 10
+# if x is greater than 30, print 'x is greater than 30'
+if x > 30:
+    print("x is greater than 30")
+
+
+money = 5
+apple_price = 2
+# if money is equal to or greater than apple_price, print 'You can buy an apple'
+if money >= apple_price:
+    print("You can buy an apple")
+    
+
