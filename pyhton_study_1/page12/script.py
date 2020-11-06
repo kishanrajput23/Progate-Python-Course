800a71501d176020bbdb02fdb9c9ce47537939e5
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-06 16:53:55 +0530

    Add files via upload

---
+++ pyhton_study_1/page12/script.py
@@ -0,0 +1,9 @@
+money = 2
+apple_price = 4
+
+if money >= apple_price:
+    print('You can buy an apple')
+# When the condition above is False, print 'You do not have enough money'
+else:
+    print("You do not have enough money")
+
