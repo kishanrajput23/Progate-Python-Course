a656b8149ad92f5c0e9fb8f845891c77490f3697
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-08 19:48:47 +0530

    Add files via upload

---
+++ pyhton_study_1/page14/script.py
@@ -0,0 +1,19 @@
+x = 20
+# if x ranges from 10 to 30 inclusive, print 'x ranges from 10 to 30'
+if 10 <= x <= 30:
+    print("x ranges from 10 to 30")
+
+
+y = 60
+# if y is less than 10 or greater than 30, print 'y is less than 10 or greater than 30'
+if y < 10 or y > 30:
+    print("y is less than 10 or greater than 30")
+    
+
+
+z = 55
+# if z is not equal to 77, print 'z is not 77'
+if not z == 77:
+    print("z is not 77")
+
+
