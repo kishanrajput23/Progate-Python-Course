2ee402b6bc8185fadd487d63468cb6cd1aed1085
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-09 18:47:32 +0530

    Add files via upload

---
+++ pyhton_study_1/page15/script.py
@@ -0,0 +1,15 @@
+# Assign 2 to the apple_price variable
+apple_price = 2
+
+# Assign 5 to the count variable
+count = 5
+
+# Assign the result of apple_price * the count variable to the total_price variable
+
+total_price = apple_price * count
+# By using the count variable, print 'You will buy .. apples'
+print("You will buy " + str(count) + " apples")
+
+# By using the total_price variable, print 'The total price is .. dollars'
+print("The total price is " + str(total_price) + " dollars")
+
