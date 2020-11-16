bce7a97c3745809da7b8bbec20ca52ac1f4c5221
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-16 12:24:26 +0530

    Add files via upload

---
+++ python_study_2/page2/script.py
@@ -0,0 +1,8 @@
+# Assign a list of strings to the fruits variable
+fruits = ["apple", "banana", "orange"]
+
+# Print the element at index 0 
+print(fruits[0])
+
+# Concatenate the string and the element at index 2, and print the result
+print("I like " + fruits[2]+ "s")
