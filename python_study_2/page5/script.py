44f318d211066b01046ad911c84fcf8835dff3bf
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-17 19:06:36 +0530

    Add files via upload

---
+++ python_study_2/page5/script.py
@@ -0,0 +1,8 @@
+# Assign a dictionary to the fruits variable
+fruits = {"apple":"manzana", "orange":"naranja"}
+
+# Print the value with the key 'orange'
+print(fruits["orange"])
+
+# By using the fruits dictionary, print 'apple is ___ in Spanish'
+print("apple is " + fruits["apple"] + " in Spanish")
