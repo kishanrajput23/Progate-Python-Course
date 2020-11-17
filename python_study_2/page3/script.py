26b1453477807dd6146a36499b5df81d7430d911
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-17 18:57:26 +0530

    Add files via upload

---
+++ python_study_2/page3/script.py
@@ -0,0 +1,14 @@
+fruits = ['apple', 'banana', 'orange']
+
+# Add 'grape' to 'fruits'
+fruits.append("grape")
+
+# Print 'fruits'
+
+print(fruits)
+# Update the element at index 0
+fruits[0] = "cherry"
+
+# Print the element at index 0
+print(fruits[0])
+
