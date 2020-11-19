b34afb84239a2fed3d2cb455ae70380306971ae9
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-19 17:07:24 +0530

    Add files via upload

---
+++ python_study_2/page7/script.py
@@ -0,0 +1,6 @@
+fruits = {'apple': 'manzana', 'orange': 'naranja', 'grape': 'uva'}
+
+# With a for loop, print '___ is ___ in Spanish'
+
+for fruits_key in fruits:
+    print(fruits_key + " is " + fruits[fruits_key] + " in Spanish")
