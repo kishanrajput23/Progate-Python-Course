65c853f9b8610c36198dfba99ea0449161e7de63
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-19 17:06:11 +0530

    Add files via upload

---
+++ python_study_2/page6/script.py
@@ -0,0 +1,11 @@
+fruits = {'apple': 1, 'banana': 2, 'orange': 4}
+
+# Update the value with the key 'banana' to 3
+fruits["banana"] = 3
+
+# Add an element to fruits with the key 'grape' and the value 5
+fruits["grape"] = 5
+
+# Print fruits
+
+print(fruits)
