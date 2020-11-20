cde9e767a961dead82066137b6074205b47d3dbc
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-20 18:49:46 +0530

    Add files via upload

---
+++ python_study_2/page9/script.py
@@ -0,0 +1,8 @@
+numbers = [765, 921, 777, 256]
+for number in numbers:
+    print(number)
+    # When the number variable is 777, print '777 found, stopping loop' and exit the loop
+    if number == 777:
+        print("777 found, stopping loop")
+        break
+    
