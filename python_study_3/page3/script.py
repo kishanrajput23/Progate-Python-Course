3fe202e1b8fdc9c45739619d5b294f2cf637e310
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-23 11:50:54 +0530

    Add files via upload

---
+++ python_study_3/page3/script.py
@@ -0,0 +1,10 @@
+# Add a parameter to print_hand
+def print_hand(hand):
+    # Use the parameter to print 'You picked: ___'
+    print("You picked: " + hand)
+
+# Call print_hand with 'Rock'
+print_hand("Rock")
+
+# Call print_hand with 'Paper'
+print_hand("Paper")
