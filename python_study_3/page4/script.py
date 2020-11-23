30a1383c443105e50096f1394bc41c00b9a831a2
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-11-23 11:52:09 +0530

    Add files via upload

---
+++ python_study_3/page4/script.py
@@ -0,0 +1,10 @@
+# Add a parameter to print_hand
+def print_hand(hand, name):
+    # Change the output to '___ picked: ___'
+    print(name + " picked: " + hand)
+
+# Add a second argument to print_hand
+print_hand('Rock', "Ken the Ninja")
+
+# Add a second argument to print_hand
+print_hand('Paper', "Computer")
