17215de6aeae2994740ee61de24843c3fc01c78b
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-02 19:23:44 +0530

    Add files via upload

---
+++ python_study_4/page4/script.py
@@ -0,0 +1,14 @@
+class MenuItem:
+    pass
+
+
+menu_item1 = MenuItem()
+
+menu_item1.name = 'Sandwich'
+print(menu_item1.name)
+
+# Set the price of menu_item1 to 5
+menu_item1.price = 5
+
+# Output the price of menu_item1
+print(menu_item1.price)
