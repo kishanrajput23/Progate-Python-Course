05a7b5704ce5d751dc27ce726d7f205ee0e27a6a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-03 12:43:11 +0530

    Add files via upload

---
+++ python_study_4/page5/script.py
@@ -0,0 +1,26 @@
+class MenuItem:
+    pass
+
+
+menu_item1 = MenuItem()
+
+menu_item1.name = 'Sandwich'
+print(menu_item1.name)
+
+menu_item1.price = 5
+print(menu_item1.price)
+
+# Create an instance of the MenuItem class
+menu_item2 = MenuItem()
+
+# Set the name of menu_item2 to 'Chocolate Cake'
+menu_item2.name = "Chocolate Cake"
+
+# Output the name of menu_item2
+print(menu_item2.name)
+
+# Set the price of menu_item2 to 4
+menu_item2.price = 4
+
+# Output the price of menu_item2
+print(menu_item2.price)
