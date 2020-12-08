e2ceec3e593de2e6c7d36a9da0cd597663fce08e
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-08 18:28:55 +0530

    Add files via upload

---
+++ python_study_4/page13/menu_item.py
@@ -0,0 +1,13 @@
+# Paste the code for the MenuItem class
+class MenuItem:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    def info(self):
+        return self.name + ': $' + str(self.price)
+
+    def get_total_price(self, count):
+        total_price = self.price * count
+        return total_price
+
