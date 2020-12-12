0e0de946087294c4cc2e3c4400d694ae63d4732a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-12 11:57:48 +0530

    Add files via upload

---
+++ python_study_5/page4/script.py
@@ -0,0 +1,11 @@
+from food import Food
+from drink import Drink
+
+food1 = Food('Sandwich', 5)
+
+# Set the calorie_count variable of food1 to 330
+food1.calorie_count = 330
+
+
+# Call the calorie_info method from food1
+food1.calorie_info()
