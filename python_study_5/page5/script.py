3cc2b0ee4908e9f1a2b95f2a349420df94a8cecf
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-12 12:01:19 +0530

    Add files via upload

---
+++ python_study_5/page5/script.py
@@ -0,0 +1,8 @@
+from food import Food
+from drink import Drink
+
+food1 = Food('Sandwich', 5)
+food1.calorie_count = 330
+
+# Call the info method from food1 and output the return value
+print(food1.info())
