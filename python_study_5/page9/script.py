e064e997fde097a651f7f6493f53a099d1ce40fe
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-13 13:00:46 +0530

    Add files via upload

---
+++ python_study_5/page9/script.py
@@ -0,0 +1,10 @@
+from food import Food
+from drink import Drink
+
+food1 = Food('Sandwich', 5, 330)
+print(food1.info())
+
+# Add an argument to Drink()
+drink1 = Drink('Coffee', 3, 180)
+
+print(drink1.info())
